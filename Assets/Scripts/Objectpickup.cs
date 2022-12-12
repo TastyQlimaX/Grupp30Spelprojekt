39e2893225cba84aec2cd883a0cbe77ef64c4dfa
Tasty <64888346+TastyQlimaX@users.noreply.github.com>
2022-12-12 12:15:22 +0100

    Pickup box protoype

---
+++ Assets/Scripts/Objectpickup.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Objectpickup : MonoBehaviour
+    private PlayerInputActions PlayerInputActions;
+    private Rigidbody _capsuleRB;
+    
+    public GameObject whatIsPickable;
+    public GameObject playerPickablePoint;
+
+
+    public void Awake()
+    {
+        GetComponent<PlayerInput>();
+        PlayerInputActions = new PlayerInputActions();
+        PlayerInputActions.Player.Enable();
+        PlayerInputActions.Player.Interract.performed += Interract;
+    }
+
+    private void PickupObject()
+    {
+        whatIsPickable.transform.SetParent(playerPickablePoint.transform);
+        whatIsPickable.transform.localScale = new Vector3(1f, 1f, 1f);
+
+    }
+
+    private void Interract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            whatIsPickable.transform.SetParent(playerPickablePoint.transform);
+            whatIsPickable.transform.localPosition = new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickable"))
+        {
+            Debug.Log("tag is pickable");
+            whatIsPickable = other.gameObject;
+        } 
+    }
+}
