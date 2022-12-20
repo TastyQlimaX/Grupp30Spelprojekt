a260e1f307804d219190e345f477a9bc6c4cc962
Tasty <64888346+TastyQlimaX@users.noreply.github.com>
2022-12-20 14:15:27 +0100

    final push

--- Assets/Scripts/ButtonsScripts/ButtonScript.cs
+++ Assets/Scripts/ButtonsScripts/ButtonScript.cs
@@ -7,13 +7,17 @@ public class ButtonScript : MonoBehaviour
 {
     public bool isPressed;
     public string nameOfTag;
+    public AudioSource buttonSFX;
+    public AudioClip Pushed;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<Tags>(out var tags))
         {
             if (tags.HasTag(nameOfTag))
             {
-                isPressed = true; 
+                isPressed = true;
+                buttonSFX.clip = Pushed;
+                buttonSFX.Play();
             }
             
         }
